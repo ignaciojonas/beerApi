e516a4c28f473cdb00dbc7ce5d62d5c72e311901
Ignacio Jonas <ignaciojonas@gmail.com>
2020-12-29 15:24:16 -0300

    Use Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,21 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_201_228_210_621) do
-  create_table 'beers', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'rating'
-    t.integer 'style_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['style_id'], name: 'index_beers_on_style_id'
+ActiveRecord::Schema.define(version: 2020_12_28_210621) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "beers", force: :cascade do |t|
+    t.string "name"
+    t.integer "rating"
+    t.bigint "style_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["style_id"], name: "index_beers_on_style_id"
   end
 
-  create_table 'styles', force: :cascade do |t|
-    t.string 'name'
-    t.text 'description'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['name'], name: 'index_styles_on_name', unique: true
+  create_table "styles", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_styles_on_name", unique: true
   end
+
 end
